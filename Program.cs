09b71c0af85c489787a079f84f938d34e0001ab9
Duc.NT <311020ntd@gmail>
2024-09-25 15:29:43 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Square square = new Square();
+
+Console.WriteLine(square);
+
+square = new Square(2.3);
+
+Console.WriteLine(square);
+
+square = new Square(5.8, "yellow", true);
+
+Console.WriteLine(square);
